70e2b89a9039c6fb2de47c9c163c831f5fbf1236
Paulo André <pauloandre0011@gmail.com>
2026-02-15 16:54:20 -0300

    change the sentence

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>André Vitor, você é a criança mais maravilhosa do mundo!</h1>;
+  return <h1>André Vitor, você é a criança mais maravilhosa do mundo!!!</h1>;
 }
